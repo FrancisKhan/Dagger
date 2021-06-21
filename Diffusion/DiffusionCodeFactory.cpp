50fd760bbcbbf6ace8680b84d7ae39188f662e21
Francesco <tntfnc3@gmail.com>
2021-06-21 16:06:34 +0100

    Deleting unnecessary folders

--- Diffusion/DiffusionCodeFactory.cpp
+++
@@ -1,6 +0,0 @@
-#include "DiffusionCodeFactory.h"
-
-std::shared_ptr<BaseDiffusionCode> DiffusionCodeFactory::setDiffusionCode(Reactor &reactor, Library &library, SolverData &solverData)
-{
-  return std::make_shared<BaseDiffusionCode>(reactor, library, solverData);
-}
--- Heat/HeatCodeFactory.cpp
+++
@@ -1,6 +0,0 @@
-#include "HeatCodeFactory.h"
-
-std::shared_ptr<BaseHeatCode> HeatCodeFactory::setHeatCode(Reactor &reactor, Library &library, SolverData &solverData)
-{
-  return std::make_shared<BaseHeatCode>(reactor, library, solverData);
-}
